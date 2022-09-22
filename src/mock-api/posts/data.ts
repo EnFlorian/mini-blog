8797595779146d39244f78b3491fc3ec73f9e38b
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-09-23 00:45:50 +0200

    Chore: Added data

--- src/mock-api/posts/data.ts
+++ src/mock-api/posts/data.ts
@@ -2,21 +2,238 @@ import authorImage from "assets/images/person.jpg";
 
 export const posts = [
   {
-    id: "1",
+    id: "0",
     title: "How to use React Query",
     excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
     content:
       "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
-    tags: ["Technology"],
+    tags: ["Programming"],
     author: {
-      name: "John Doe",
+      name: "Akira Bauer",
       image: authorImage,
     },
     date: "2020 01, 2021",
     read: 5,
     coverImage: {
-      src: "https://images.unsplash.com/photo-1581084514319-8e8b6b5b5d3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      alt: "How to use React Query",
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "1",
+    title: "Is the bigger screen on the new nintendo switch worth it?",
+    excerpt:
+      "The Nintendo Switch is a great console, but it's not without its flaws. One of the biggest is the screen size.",
+    content:
+      "The Nintendo Switch is a great console, but it's not without its flaws. One of the biggest is the screen size. It's not as big as the one on the Wii U, and it's not as big as the one on the 3DS. The Switch's screen is also smaller than the one on the PS Vita, which is a shame because the Vita is a great handheld console. The Switch's screen is also smaller than the one on the PS Vita, which is a shame because the Vita is a great handheld console.",
+    tags: ["Gaming"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "2",
+    title: "What does the future of electric mobiles look like?",
+    excerpt: "The future of electric mobiles is bright, but it's not without its challenges.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Technology"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "3",
+    title: "How version control is properly done in a team?",
+    excerpt: "Version control is a great tool for teams, but it's not without its challenges.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Programming"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "4",
+    title: "How many monitors are too many monitors?",
+    excerpt: "The number of monitors you need depends on what you're doing.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Technology"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "5",
+    title: "How gaming has changed over the last 10 years",
+    excerpt: "Gaming has changed a lot over the last 10 years, and it's not without its challenges.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Gaming"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "6",
+    title: "What makes processor architecture different from GPU?",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Technology"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "7",
+    title: "How robots will change our daily lives in the future",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Technology"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "8",
+    title: "How long will it take us to replace conventioanl cars with electric ones?",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Technology"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "9",
+    title: "How can you jailbreak your nintendo switch?",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Gaming"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "10",
+    title: "Should you use a GPU or CPU for your machine learning?",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Programming"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "11",
+    title: "What are robots already capable of?",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Technology"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
+    },
+  },
+  {
+    id: "12",
+    title: "Does using dark modes really reduce the strain on our eyes?",
+    excerpt: "React Query is a library that helps you fetch, cache and update data in your React applications.",
+    content:
+      "React Query is a library that helps you fetch, cache and update data in your React applications. It will automatically keep your UI in sync with the latest data and reduce the amount of boilerplate in your application.",
+    tags: ["Programming"],
+    author: {
+      name: "Akira Bauer",
+      image: authorImage,
+    },
+    date: "2020 01, 2021",
+    read: 5,
+    coverImage: {
+      src: "../assets/images/posts/7.jpg",
+      alt: "Computer Screen",
     },
   },
 ];
